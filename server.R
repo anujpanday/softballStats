49a4c7efba3a80f73cb7857f0f60a02043bb7f6b
Panday <apanday@cardlytics.com>
2017-03-31 11:47:02 -0400

    made axis labels better

--- server.R
+++ server.R
@@ -4,8 +4,7 @@ server<-function(input, output) {
     ggplot(data=stats, aes_string(x=Players, y=input$metric, 
                                   fill=stats$Players))+
       geom_bar(stat='identity')+
-      
-      theme(legend.position='none', axis.title.x=element_blank())
+      theme(legend.position='none', axis.title.x=element_blank(), axis.text=element_text(size=12, face='bold'))
     
     
   })
