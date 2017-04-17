9ec139c849cc89fc3d5f777bab7b24e6b8f098f9
Anuj Panday <apanday@cardlytics.com>
2017-04-17 16:56:15 -0400

    added games 2 and 3 and added OPS

--- server.R
+++ server.R
@@ -2,12 +2,17 @@ stats<-read.csv('softballStats.csv')
 
 server<-function(input, output) {
   attach(stats)
+  
   output$chart<-renderPlot({
-    ggplot(data=stats, aes_string(x=Players, y=input$metric, 
+    ggplot(data=stats, aes_string(x=stats$Players, y=input$metric, 
                                   fill=stats$Players))+
       geom_bar(stat='identity')+
       theme(legend.position='none', axis.title.x=element_blank(), axis.text=element_text(size=12, face='bold'))
-    
-    
   })
+  
+  output$blank<-renderText('Data Table')
+  output$table<-renderDataTable(stats)
+ 
 }
+
+
--- ui.R
+++ ui.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(ggplot2)
+library(rJava)
 library(xlsx)
+
 
 #setwd("C:/Users/apanday/Dropbox")
 
@@ -10,5 +12,7 @@ library(xlsx)
 
 shinyUI(fluidPage( 
   selectInput('metric', 'Select Metric', c('G','AB', 'HR', 'X2B', 'X3B', 'BA', 'SLG', 'OPS'), selected='OPS'),
-  plotOutput('chart')
+  plotOutput('chart'),
+  textOutput('blank'),
+  dataTableOutput('table')
 ))
